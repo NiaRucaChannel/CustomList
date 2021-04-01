d012506a221d05d3bbdd8482da5413cf3ac2629f
NiaRuca@Spice <NiaRucaChannel@spice-group.jp>
2021-04-01 19:30:13 +0900

    Added CustomList.cs and CustomListEditor.cs

---
+++ CustomList.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class CustomList : MonoBehaviour
+{
+    [SerializeField] int num = 1;
+    [SerializeField] List<int> sampleList;
+
+}
---
+++ CustomListEditor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(CustomList))]
+public class CustomListEditor : Editor
+{
+
+    SerializedProperty sampleList;
+    SerializedProperty num;
+    void OnEnable()
+    {
+        sampleList = serializedObject.FindProperty("sampleList");
+        sampleList.isExpanded = true;
+        num = serializedObject.FindProperty("num");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(num);
+        sampleList.arraySize = num.intValue;
+        EditorGUILayout.PropertyField(sampleList);
+        serializedObject.ApplyModifiedProperties();
+    }
+}
